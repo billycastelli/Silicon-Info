56dae8fcca67c593de059aa35d7e6fd361af26d3
Billy Castelli <billycastelli@gmail.com>
2020-11-23 23:00:47 -0800

    Use label

--- silicon-info/ContentView.swift
+++ silicon-info/ContentView.swift
@@ -23,7 +23,6 @@ struct ContentView: View {
     }
 }
 
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(appName: "", architecture: "", appIcon: NSImage(imageLiteralResourceName: "ProcessorIcon"))
